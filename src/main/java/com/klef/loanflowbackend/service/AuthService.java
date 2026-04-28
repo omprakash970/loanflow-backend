2fa355653aecfce22406bae85109278ca827121f
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-04-28 14:04:55 +0530

    done

--- src/main/java/com/klef/loanflowbackend/service/AuthService.java
+++ src/main/java/com/klef/loanflowbackend/service/AuthService.java
@@ -10,6 +10,7 @@ import com.klef.loanflowbackend.repository.UserRepository;
 import com.klef.loanflowbackend.security.JwtService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
